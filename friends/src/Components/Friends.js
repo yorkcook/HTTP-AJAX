a4b58988a14bd251bb2990bab32bfc5c9486cb28
York Cook <yorkcook@Johnny-Castle.lan1>
2019-06-12 15:34:38 -0400

    WIP: setting up api call

--- friends/src/Components/Friends.js
+++ friends/src/Components/Friends.js
@@ -1,4 +1,23 @@
 import React from "react";
 import axios from "axios";
 
+class Friends extends React.Component {
+    constructor(){
+        super();
+        this.state={
+            friends =[]
+        }
+    }
+}
+
+componentDidMount() {
+    axios.get('http://localhost:5000')
+    .then(res => {
+        console.log(res)
+    })
+    .catch( err => {
+        console.log(err)
+    })
+}
+
 export default Friends;
